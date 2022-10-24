e6ad09b3e4664ad28a8ba508996f3782d188cdbd
joeephwild <josephomotade0@gmail.com>
2022-10-24 15:41:16 +0100

    first commit

--- backend/truffle-config.js
+++ backend/truffle-config.js
@@ -1,6 +1,6 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
+
+ /** Use this file to configure your truffle project. It's seeded with some
+ *common settings for different networks and features like migrations,
  * compilation, and testing. Uncomment the ones you need or modify
  * them to suit your project as necessary.
  *
@@ -26,7 +26,7 @@
  * For example, your .env file will have the following structure:
  *
  * MNEMONIC = <Your 12 phrase mnemonic>
- * PROJECT_ID = <Your Infura project id>
+ * 
  *
  * Deployment with Truffle Dashboard (Recommended for best security practice)
  * --------------------------------------------------------------------------
@@ -40,11 +40,12 @@
  *
  * https://trufflesuite.com/docs/truffle/getting-started/using-the-truffle-dashboard/
  */
+ 
 
-// require('dotenv').config();
-// const { MNEMONIC, PROJECT_ID } = process.env;
+require('dotenv').config();
+const { MNEMONIC, PROJECT_ID } = process.env;
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+ const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   /**
@@ -67,7 +68,7 @@ module.exports = {
     // development: {
     //  host: "127.0.0.1",     // Localhost (default: none)
     //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
+    // network_id: "*",       // Any network (default: none)
     // },
     //
     // An additional network, but with some advanced options…
@@ -75,28 +76,28 @@ module.exports = {
     //   port: 8777,             // Custom port
     //   network_id: 1342,       // Custom network
     //   gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    //   gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+      //gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
     //   from: <address>,        // Account to send transactions from (default: accounts[0])
     //   websocket: true         // Enable EventEmitter interface for web3 (default: false)
     // },
     //
     // Useful for deploying to a public network.
-    // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // goerli: {
-    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
-    //   network_id: 5,       // Goerli's id
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-    //
-    // Useful for private networks
+     //Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
+     goerli: {
+     provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
+      network_id: 5,       // Goerli's id
+      confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+    
+     //Useful for private networks
     // private: {
-    //   provider: () => new HDWalletProvider(MNEMONIC, `https://network.io`),
-    //   network_id: 2111,   // This network is yours, in the cloud.
-    //   production: true    // Treats this network as if it was a public net. (default: false)
+      //provider: () => new HDWalletProvider(MNEMONIC, `https://network.io`),
+     // network_id: 2111,   // This network is yours, in the cloud.
+      // production: true    // Treats this network as if it was a public net. (default: false)
     // }
-  },
+ },
 
   // Set default mocha options here, use special reporters, etc.
   mocha: {
